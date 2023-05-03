ce3eecdff86bf1e3acab0b2c1f37f03473baed4f
Dun Mao <moderndelta137@gmail.com>
2023-05-04 02:13:05 +0900

    Simple Control for player sub
    Created some simple scripts to move the player sub and camera

--- Assets/Scripts/PlayerScripts/MainCameraManager.cs
+++ Assets/Scripts/PlayerScripts/MainCameraManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MainCameraManager : MonoBehaviour
 {
+    public GameObject LockonTarget;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class MainCameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //A simple test script to make the camera follow the PlayerSub only on x axis
+        this.transform.position=new Vector3( LockonTarget.transform.position.x,this.transform.position.y,this.transform.position.z);
     }
 }
--- Assets/Scripts/PlayerScripts/PlayerSubController.cs
+++ Assets/Scripts/PlayerScripts/PlayerSubController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerSubController : MonoBehaviour
 {
+    public float Defalut_SubMoveForwardSpeed;
+    private float actual_SubMoveForwardSpeed;
+    public float Defalut_SubMoveSidewaySpeed;
+    private float actual_SubMoveSidewaySpeed;
+    public bool CanMove; //Use to determine if the sub can move at all;
+    public bool CanControl; // Use to determine if the player can control the movement of the sub;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Initialize
+        actual_SubMoveForwardSpeed=Defalut_SubMoveForwardSpeed;
+        actual_SubMoveSidewaySpeed=Defalut_SubMoveSidewaySpeed;
+        CanMove=true;
+        CanControl=true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(CanMove)
+        {
+            MoveSubForward();
+            if(CanControl)
+            {
+                this.gameObject.transform.Translate(Vector3.up*Input.GetAxis("Vertical")*actual_SubMoveSidewaySpeed*Time.deltaTime,Space.Self);
+            }
+        }
+    }
+
+    // Make the sub move forward every frame
+    private void MoveSubForward()
+    {
+        this.gameObject.transform.Translate(Vector3.right*actual_SubMoveForwardSpeed*Time.deltaTime,Space.Self);
     }
 }
